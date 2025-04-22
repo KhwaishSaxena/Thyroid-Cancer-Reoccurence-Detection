0dbd9dd9560a4dbcf51deb6c5fc307925e92d6b0
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-04-22 10:42:18 +0530

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     pg = st.navigation([
         st.Page('about.py', title = "Home", icon = ':material/home:'),
-        st.Page('prediction.py', title = "Predict", icon = ':material/stethoscope:'),
+        #st.Page('prediction.py', title = "Predict", icon = ':material/stethoscope:'),
     ])
     pg.run()
     
