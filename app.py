248a7531dca372700b914cdbe16de9f53de69cef
Khwaish Saxena <160560474+KhwaishSaxena@users.noreply.github.com>
2025-04-22 10:40:11 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    pg = st.navigation([
+        st.Page('about.py', title = "Home", icon = ':material/home:'),
+        st.Page('prediction.py', title = "Predict", icon = ':material/stethoscope:'),
+    ])
+    pg.run()
+    
+if __name__  == "__main__":
+    main()
